b4b5d1d146167503ad288d0adca92fbf136224ed
= <=>
2023-03-15 15:58:56 +0100

    modified:   index.html 	modified:   static/css/style.css 	modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
+const nav = document.querySelector('nav')
 document.addEventListener('scroll',function(){
-
+    console.log(window.scrollY)
+    if(window.scrollY > 174){
+        nav.style.backgroundColor = 'rgba(0,0,0,0.8)' 
+        nav.style.width = '100%' 
+    }else{
+        nav.style.backgroundColor = 'rgba(0,0,0,0)' 
+    }
 })
 
