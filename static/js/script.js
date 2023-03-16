06ec592106cdf1b0e2b3e5dcb25adbf5bf18d46d
divineamunega <divineamunega@gmail.com>
2023-03-16 19:42:27 +0100

    modified:   index.html 	modified:   static/css/style.css 	new file:   static/img/discover.jpg 	modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,12 @@ document.addEventListener('scroll',function(){
     if(window.scrollY > 174){
         nav.style.backgroundColor = 'rgba(0,0,0,0.8)' 
         nav.style.width = '100%' 
+        nav.style.backdropFilter = 'blur(10px)'
+        nav.style.height = 'fit-content'
     }else{
+        nav.style.width = '90%' 
+        nav.style.height = '13vh'
+        nav.style.backdropFilter = 'blur(0)'
         nav.style.backgroundColor = 'rgba(0,0,0,0)' 
     }
 })
