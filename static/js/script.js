116e9d6accd8bf9fc2c7d924c39e25955ade51c2
divinethecoder <divineamunega1@gmail.com>
2023-03-18 22:06:10 +0100

    modified:   index.html 	modified:   static/css/style.css 	modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,15 @@
-const nav = document.querySelector('nav')
-document.addEventListener('scroll',function(){
-    console.log(window.scrollY)
-    if(window.scrollY > 174){
-        nav.style.backgroundColor = 'rgba(0,0,0,0.8)' 
-        nav.style.width = '100%' 
-        nav.style.backdropFilter = 'blur(10px)'
-        nav.style.height = 'fit-content'
-    }else{
-        nav.style.width = '90%' 
-        nav.style.height = '13vh'
-        nav.style.backdropFilter = 'blur(0)'
-        nav.style.backgroundColor = 'rgba(0,0,0,0)' 
-    }
-})
-
+const nav = document.querySelector("nav");
+document.addEventListener("scroll", function () {
+  // console.log(window.scrollY)
+  if (window.scrollY > 174) {
+    nav.style.backgroundColor = "rgba(0,0,0,0.89)";
+    nav.style.width = "100%";
+    // nav.style.backdropFilter = "blur(10px)";
+    nav.style.height = "fit-content";
+  } else {
+    nav.style.width = "90%";
+    nav.style.height = "13vh";
+    // nav.style.backdropFilter = "blur(0)";
+    nav.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+});
